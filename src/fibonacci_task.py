36ae13a56b8c0025716e710d014b24b6bc41b7d3
Andrey Marinov <heavenlymage97@gmail.com>
2022-10-03 17:11:01 +0300

    Program that checks the Fibonacci numbers

--- src/fibonacci_task.py
+++ src/fibonacci_task.py
@@ -1,19 +1,22 @@
 '''Modul for checking the Fibonacci number'''
 
 
-def get_fibonacci_num(num):
-    '''Function that returns the Fibonacci num'''
+def get_fibonacci_num(num) -> int:
+    '''Function that returns the Fibonacci num
+    
+    :param: num:  value that is used to calculate the fibonacci number
+    :return: int: when calculating the fibonacci number
+    '''
     return num if num in (0,1) else get_fibonacci_num(num-1) + get_fibonacci_num(num-2)
 
-print(get_fibonacci_num(10))
 
-
-def get_fibonacci_first_ten_numbers():
-    '''Function that returns the fibonacci numbers from 0 to 10 and append them to list'''
+def get_fibonacci_first_ten_numbers() -> list:
+    '''Function that returns the fibonacci numbers from 0 to 10 and append them to list
+    
+    :return: list with the first 10 fibonacci numbers
+    '''
     num = 10
     fibonacci = [0, 1]
     fibonacci += [(fibonacci := [fibonacci[1], fibonacci[0]
                     + fibonacci[1]]) and fibonacci[1] for k in range(num-2)]
     return fibonacci
-
-print (get_fibonacci_first_ten_numbers())
